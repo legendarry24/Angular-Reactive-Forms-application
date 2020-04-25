024c73033eec74d0d74aa01dd66a81bc0d9cde9e
legendarry24 <ihor.olkhovskyi@nure.ua>
2020-04-25 21:11:00 +0300

    fix lint errors by running "ng lint --fix" command

--- APM/src/app/products/product-edit/product-edit.guard.spec.ts
+++ APM/src/app/products/product-edit/product-edit.guard.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ProductEditGuard } from './product-edit.guard';
 
 describe('ProductEditGuard', () => {
-  let guard: ProductEditGuard;
+	let guard: ProductEditGuard;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(ProductEditGuard);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		guard = TestBed.inject(ProductEditGuard);
+	});
 
-  it('should be created', () => {
-    expect(guard).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(guard).toBeTruthy();
+	});
 });
